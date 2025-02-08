f461aba7f11cf1785e00aa69b7f5342293da6c2d
vikas jamdar <vikas.jamdar@corecotechnologies.com>
2025-02-08 18:23:07 +0530

    refactor: Backend should allow cors headers and cross origin request

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
